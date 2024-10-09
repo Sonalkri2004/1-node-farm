79c1a50d144d58806a66e612a0822d008c484ebb
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-10-09 22:25:46 +0530

    building a simple api.

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,11 @@
 //SERVER
+const fs=require('fs');
 const http = require('http');
 const url = require('url');
+
+const data=fs.readFilesync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj=JSON.parse(data);
+
 const server=http.createServer((req,res)=>{
     const pathName=req.url;
     if(pathName==='/' || pathName==='/overview'){
@@ -9,6 +14,12 @@ const server=http.createServer((req,res)=>{
     else if(pathName==='/product'){
         res.end("hello from server this is a product page");
 
+    }
+    else if(pathName==='/api'){
+        
+       res.end(data);
+        
+       
     }
     else{
         res.writeHead(404)
